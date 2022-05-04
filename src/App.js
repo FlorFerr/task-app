10c1246c08da274777065669d3a577eae77a1c4a
Flor <florferreyra16@gmail.com>
2022-05-04 16:05:02 -0300

    custom hook en el GET

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React, { useEffect, useState } from 'react';
+import useHttp from './Hooks/useHttp';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  
   const [tasks, setTasks] = useState([]);
 
-  const fetchTasks = async (taskText) => {
+  const transformTasks = (taskObj) => {
+    const loadedTasks = [];
+
+      for (const taskKey in taskObj) {
+        loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
+      }
+
+      setTasks(loadedTasks);
+
+  }
+  const {sendRequest: fetchTasks, isLoading, error} = useHttp({url: 'https://moviesapp-10288-default-rtdb.firebaseio.com/tasks.json'}, transformTasks)
+
+  /*const fetchTasks = async (taskText) => {
     setIsLoading(true);
     setError(null);
     try {
@@ -33,7 +45,7 @@ function App() {
       setError(err.message || 'Something went wrong!');
     }
     setIsLoading(false);
-  };
+  };*/
 
   useEffect(() => {
     fetchTasks();
